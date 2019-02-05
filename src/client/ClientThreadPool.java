6d8eb7ad217ae29049e34a07c2ff848326e64db3
DTang <david.tang@ucalgary.ca>
2019-02-04 23:48:42 -0700

    made edits

---
+++ src/client/ClientThreadPool.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+public class ClientThreadPool {
+	private PrintWriter socketOut;
+	private Socket aSocket;
+	private BufferedReader stdIn;
+	private BufferedReader socketIn;
+
+	public ClientThreadPool(String serverName, int portNumber) {
+		try {
+			aSocket = new Socket(serverName, portNumber);
+			stdIn = new BufferedReader(new InputStreamReader(System.in));
+			socketIn = new BufferedReader(new InputStreamReader(aSocket.getInputStream()));
+			socketOut = new PrintWriter((aSocket.getOutputStream()), true);
+		} catch (IOException e) {
+			System.err.println(e.getStackTrace());
+		}
+	}
+
+	public void communicate() {
+		String line = "hello";
+		String response = "";
+		while (!line.equals("QUIT")) {
+			try {
+
+				System.out.println("please enter a word: ");
+				line = stdIn.readLine();
+				socketOut.println(line);
+				response = socketIn.readLine();
+				System.out.println(response);
+			} catch (IOException e) {
+				System.out.println("Sending error: " + e.getMessage());
+			}
+			catch (Exception e)
+			{
+				System.out.println("what is going on " + e.getMessage());
+				break;
+			}
+		}
+		try {
+			stdIn.close();
+			socketIn.close();
+			socketOut.close();
+		} catch (IOException e) {
+			System.out.println("closing error: " + e.getMessage());
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		ClientThreadPool aClient = new ClientThreadPool("localhost", 9898);
+		aClient.communicate();
+	}
+}
